408140da77212d57d693aeab999f7b1e290bd004
Tyler Bierwirth <tylerbierwirth@gmail.com>
2024-02-29 11:40:05 -0700

    Add business logic for calculating price

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,10 @@ class Booking < ApplicationRecord
     validates :first_name, :last_name, :animal_name, :animal_type, :hours_requested, :date_of_service, presence: true
     validates :hours_requested, numericality: { greater_than_or_equal_to: 2, less_than_or_equal_to: 8 }
     validates :animal_type, inclusion: { in: %w[Dog Cat] }
+
+    def calculate_price
+        base_price = 20
+        additional_charge = animal_type == 'Dog' ? 10 : 5
+        base_price + (hours_requested * additional_charge)
+    end
 end
