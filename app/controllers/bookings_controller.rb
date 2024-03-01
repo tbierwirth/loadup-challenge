d6b4a0cc7014c771d4b09e4a7a0bb28db4de0e14
Tyler Bierwirth <tylerbierwirth@gmail.com>
2024-02-29 17:28:05 -0700

    Add bootstrap flash messages

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,27 +1,27 @@
 class BookingsController < ApplicationController
     before_action :authenticate_user!, except: [:new, :create]
     before_action :check_admin, only: [:index]
-    
+
     def new
       @booking = Booking.new
     end
-  
+
     def create
       @booking = Booking.new(booking_params)
       if @booking.save
-        flash[:notice] = 'Booking Created!'
+        flash[:success] = 'Booking Created!'
         redirect_to root_path
       else
         render :new
       end
     end
-  
+
     def index
       @bookings = Booking.all
     end
-  
+
     private
-  
+
     def booking_params
       params.require(:booking).permit(:first_name, :last_name, :animal_name, :animal_type, :hours_requested, :date_of_service)
     end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def bootstrap_flash_class(type)
+    case type.to_sym
+    when :notice
+      "alert-info"
+    when :success
+      "alert-success"
+    when :error
+      "alert-danger"
+    when :alert
+      "alert-warning"
+    else
+      "alert-primary"
+    end
+  end
 end
